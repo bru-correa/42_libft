6f08a1c1970120a790f44217c6810f3522a65b38
bru-correa <bruuh.cor@gmail.com>
2021-09-15 23:17:35 -0300

    Added 'include libft.h' to some files

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,14 +6,25 @@
 /*   By: bcorrea- <bruuh.cor@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/03 19:18:26 by bcorrea-          #+#    #+#             */
-/*   Updated: 2021/09/05 15:09:47 by bcorrea-         ###   ########.fr       */
+/*   Updated: 2021/09/15 22:35:05 by bcorrea-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+/*
 int	ft_isalnum(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') \
 		|| (c >= '0' && c <= '9'))
+		return (1);
+	return (0);
+}
+*/
+
+int	ft_isalnum(int c)
+{
+	if (ft_isdigit(c) || ft_isalpha(c))
 		return (1);
 	return (0);
 }
