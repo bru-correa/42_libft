89714e2dbdd38e7a409397c489af26381aa203b9
bru-correa <bruuh.cor@gmail.com>
2021-09-22 13:25:03 -0300

    Added strteri function

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,15 +6,22 @@
 /*   By: bcorrea- <bruuh.cor@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/21 17:45:44 by bcorrea-          #+#    #+#             */
-/*   Updated: 2021/09/22 13:15:10 by bcorrea-         ###   ########.fr       */
+/*   Updated: 2021/09/22 13:24:18 by bcorrea-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/*
 void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
+	unsigned int	i;
 
+	if (!s)
+		return ;
+	i = 0;
+	while (s[i])
+	{
+		f(i, &s[i]);
+		i++;
+	}
 }
-*/
