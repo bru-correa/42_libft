f3955d6bbbb4d7f89ca5299907bfb559856f13fd
bru-correa <bruuh.cor@gmail.com>
2021-09-22 18:49:55 -0300

    Added itoa and putnbr functions

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,15 +6,36 @@
 /*   By: bcorrea- <bruuh.cor@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/21 16:19:04 by bcorrea-          #+#    #+#             */
-/*   Updated: 2021/09/22 13:14:12 by bcorrea-         ###   ########.fr       */
+/*   Updated: 2021/09/22 18:47:50 by bcorrea-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/*
+static void	write_digit(unsigned int n, int fd);
+
 void	ft_putnbr_fd(int n, int fd)
 {
+	unsigned int	un;
 
+	if (n >= 0)
+	{
+		un = (unsigned int) n;
+	}
+	else
+	{
+		un = (unsigned int)(n * -1);
+		write(fd, (char *) '-', 1);
+	}
+	write_digit(un, fd);
 }
-*/
+
+static void	write_digit(unsigned int n, int fd)
+{
+	char	c;
+
+	if (n / 10 > 0)
+		write_digit(n / 10, fd);
+	c = (n % 10) + '0';
+	write(fd, &c, 1);
+}
