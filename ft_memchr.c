a939e5ba5790691235414f629deabe0c3aa33886
bru-correa <bruuh.cor@gmail.com>
2021-09-29 19:33:05 -0300

    Fixed ft_memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: bcorrea- <bruuh.cor@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/09 17:32:45 by bcorrea-          #+#    #+#             */
-/*   Updated: 2021/09/15 21:28:52 by bcorrea-         ###   ########.fr       */
+/*   Updated: 2021/09/29 19:32:41 by bcorrea-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,18 @@ void	*ft_memchr(const void *s, int c, size_t n)
 {
 	unsigned char	*str;
 	unsigned char	chr;
+	int				i;
 
-	str = (unsigned char *) s;
-	chr = (unsigned char) c;
 	if (n == 0)
 		return (NULL);
-	while (*str && n--)
+	str = (unsigned char *) s;
+	chr = (unsigned char) c;
+	i = 0;
+	while ((void *)(s + i) && n--)
 	{
-		if (*str == chr)
-			return ((void *) str);
-		str++;
+		if (str[i] == chr)
+			return ((void *)(s + i));
+		i++;
 	}
-	if (!*str && !chr)
-		return ((void *) str);
 	return (NULL);
 }
