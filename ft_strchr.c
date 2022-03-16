b18ed73b91b8a3223d949425c4675e21b9f81f9c
bru-correa <bruuh.cor@gmail.com>
2022-03-16 06:56:50 +0100

    fix: added an extra protection

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: bcorrea- <bruuh.cor@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/09 15:37:31 by bcorrea-          #+#    #+#             */
-/*   Updated: 2021/09/09 15:53:27 by bcorrea-         ###   ########.fr       */
+/*   Updated: 2022/03/16 06:55:22 by bcorrea-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 char	*ft_strchr(const char *s, int c)
 {
+	if (!s)
+		return (NULL);
 	if (c > 127)
 		c %= 256;
 	while (*s)
